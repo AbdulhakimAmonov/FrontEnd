17f289d835acfb9dc10ae54905c73b8ec39b68a4
AbdulhakimUS <127390486+AbdulhakimAmonov@users.noreply.github.com>
2023-11-04 22:05:44 +0500

    Add files via upload

---
+++ DOM/app.js
@@ -0,0 +1,39 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// document.querySelector('h1').innerHTML = fruits;
+// document.getElementsByTagName('li')[1].innerHTML = "Komron";
+// document.getElementsByTagName('li')[1].innerHTML = fruits;
+
+// var ism = prompt();
+// var array = ["Alice" , "Bob"];
+// for (let i = 0; i < array.length; i++) {
+//     if (ism === array[i]) {
+//         console.log("Salom " + ism);
+//         break;
+//     } else {
+//         console.log("Salom notanish");
+//     }
+// }
+
+  
+// 1
+// document.querySelector('#demo').innerHTML = 'hello dom'
+
+// 2
+// document.querySelector('#box').style.background = 'red'
+
+// 3
+// document.getElementsByTagName('li')[1].style.fontSize = '50px'
+
+// 4
+// document.addEventListener("DOMContentLoaded" , function () {
+//     var div = document.getElementById("div");
+//         var tagName = div.tagName;
+//         var tagClass = div.getAttribute("class");
+//         var tagId = div.getAttribute("id");
+//         alert("Tag name " + tagName + "n" + "Tag class value " + tagClass + "n" + "Tag id value " + tagId);
+//     }
+// )
+
+// document.getElementById('demo').classList.add('demo')
+// document.getElementById('demo').classList.remove('demo')
+
